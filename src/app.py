d9c1eb73f0a8ab445964f8589377492e3f274f65
urizar-ignacio <urizar.ignacio@gmail.com>
2023-05-11 02:25:22 -0400

    feat: create reports endpoints

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
-from src.gateway.endpoint import Department, HiredEmployee, Jobs
+from src.gateway.endpoint import Department, HiredEmployee, Jobs, EmployeesByQuarter, EmployeesOverMean
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,9 +10,14 @@ api = Api(app)
 def hello_world():
     return "<p>Hello, World!!!</p>"
 
+# DOMAINS
 api.add_resource(Department, '/api/departments/')
 api.add_resource(HiredEmployee, '/api/hired_employees/')
 api.add_resource(Jobs, '/api/jobs/')
+
+# REPORTS
+api.add_resource(EmployeesByQuarter, '/api/employees_by_quarter/')
+api.add_resource(EmployeesOverMean, '/api/employees_over_mean/')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/usecase/get_report.py
@@ -0,0 +1,3 @@
+def get_report(repo, report_name):
+    results = repo.get_report(report_name)
+    return results

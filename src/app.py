a122dad5ff7808658b58a3234571c373fec04f9d
iurizar-smu <iurizar@smu.cl>
2023-05-11 00:14:17 -0400

    add flask_restful to app setup

--- src/app.py
+++ src/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_restful import Api
+
+from src.gateway.endpoint import Jobs
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!!!</p>"
+
+api.add_resource(Jobs, '/api/jobs/')
 
 if __name__ == "__main__":
     app.run(debug=True)
